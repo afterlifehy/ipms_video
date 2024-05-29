8b4a9ee32d1d8add244f051d66955b727558b28c
afterlifehy <757198090@qq.com>
2024-05-29 11:48:04 +0800

    2.4.4_alpha 催缴管理路段名称不可选择

--- base/src/main/java/com/peakinfo/base/base/mvvm/UrlManager.kt
+++ base/src/main/java/com/peakinfo/base/base/mvvm/UrlManager.kt
@@ -1,10 +1,12 @@
 package com.peakinfo.base.base.mvvm
+
 import com.peakinfo.base.BuildConfig
 
 object UrlManager {
-//    const val DEV_HOST = "http://10.0.0.73:8080/ipms/service/"
+    //    const val DEV_HOST = "http://10.0.0.73:8080/ipms/service/"
     const val DEV_HOST = "http://180.169.37.244/ipms/service/"
     const val FORMAL_HOST = "http://ipms.csnits.com/ipms/service/"
+//    const val DEV_HOST = "http://csnits.com/JingAnManagerApp/service/"
 
     fun getServerUrl(): String {
         if (BuildConfig.is_dev) {
