9c75fcf4b101d17f35f872e3a591f72cbe9e9d3d
afterlifehy <757198090@qq.com>
2024-06-05 15:51:55 +0800

    2.4.4_alpha 催缴管理路段名称不可选择

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,7 +5,7 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 30015
+    const val versionCode = 30016
     const val versionName = "2.4.4_beta"
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
