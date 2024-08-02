ff0eb7a20a885d6d0d1978dd045f485c891e8833
afterlifehy <757198090@qq.com>
2024-08-02 09:51:31 +0800

    2.4.5_beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,7 +105,7 @@ android {
             if (this is com.android.build.gradle.internal.api.ApkVariantOutputImpl) {
                 if (buildType.name == "release") {
                     this.outputFileName =
-                        "plateid_v${versionName}_${versionCode}_.apk"
+                        "plateid_v${versionName}_${versionCode}.apk"
                 } else {
                     this.outputFileName =
                         "plateid_v${versionName}_${versionCode}_debug_${BuildConfig.debug_is_dev}.apk"
--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,8 +5,8 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 30017
-    const val versionName = "2.4.5_alpha"
+    const val versionCode = 30018
+    const val versionName = "2.4.5_beta"
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
