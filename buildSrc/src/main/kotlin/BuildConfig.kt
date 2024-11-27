28cca9f05c1dd8ec9f4fdec411b156f549be8b3a
afterlifehy <757198090@qq.com>
2024-11-27 13:46:26 +0800

    2.4.6 30027

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,13 +5,13 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 30025
-    const val versionName = "2.4.6_alpha"
+    const val versionCode = 30027
+    const val versionName = "2.4.6"
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     const val debug_is_debug = "true"
-    const val debug_is_dev = "true"
+    const val debug_is_dev = "false"
     const val debug_is_proxy = "true"
 
     const val release_is_debug = "false"
