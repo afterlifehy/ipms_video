176d4c4770572b1dbf5ef10cc5224f6ef445e639
afterlifehy <757198090@qq.com>
2025-03-19 09:46:50 +0800

    2.4.7 30030 交易查询，orderType=1时，显示场内支付 异常上报当异常类型为03或04时，等待上报结果，不直接返回上一页 欠费订单详情、营收盘点、在停泊位详情、预支付、交易查询、交易记录打印告知书时，先连接打印机，再打印

--- base/src/main/java/com/peakinfo/base/base/mvvm/UrlManager.kt
+++ base/src/main/java/com/peakinfo/base/base/mvvm/UrlManager.kt
@@ -5,7 +5,7 @@ import com.peakinfo.base.BuildConfig
 object UrlManager {
     //    const val DEV_HOST = "http://10.0.0.73:8080/ipms/service/"
     const val DEV_HOST = "http://114.94.20.110/ipms/service/"
-    const val FORMAL_HOST = "http://ipms.csnits.com/ipms/service/"
+    const val FORMAL_HOST = "http://ipms.rentongtek.com/ipms/service/"
 //    const val DEV_HOST = "http://csnits.com/JingAnManagerApp/service/"
 
     fun getServerUrl(): String {
