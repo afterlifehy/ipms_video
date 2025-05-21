ffd6786a2c216ec54d376b828b6801082f9cc73c
afterlifehy <757198090@qq.com>
2025-05-21 14:43:20 +0800

    2.4.10_alpha 30039 登录成功后，开启一个前台服务，每隔15分钟发生心跳

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,8 +5,8 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 30037
-    const val versionName = "2.4.10_beta"
+    const val versionCode = 30039
+    const val versionName = "2.4.11_alpha"
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
