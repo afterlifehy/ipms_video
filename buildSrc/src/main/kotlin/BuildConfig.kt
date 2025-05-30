c76a0d9365d38c076e8af1a1f761feda6f359629
afterlifehy <757198090@qq.com>
2025-05-30 09:03:00 +0800

    2.4.10_beta 30039 登录成功后，开启一个前台服务，每隔15分钟发生心跳 包名改回com.peakinfo.plateid，和2.4.8一致

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,8 +5,8 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 30039
-    const val versionName = "2.4.10_beta"
+    const val versionCode = 30040
+    const val versionName = "2.4.10_beta2"
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
