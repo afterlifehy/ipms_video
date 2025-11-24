18b2139247f5679fdb9a5ed074055bf16be9cfe9
afterlifehy <757198090@qq.com>
2025-11-24 15:53:12 +0800

    2.4.12 30050 每次进入签到页，检查更新、 在我的界面新增日志入口，选择对应日志进行上传

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,8 +5,8 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 30049
-    const val versionName = "2.4.12_beta4"
+    const val versionCode = 30050
+    const val versionName = "2.4.12"
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
